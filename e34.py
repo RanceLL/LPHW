0981df627504f5fa9712975a27b5dec9601db651
rance <vergil000@gmail.com>
2017-02-23 23:09:51 +0900

    e34

---
+++ e34.py
@@ -0,0 +1,4 @@
+#Q2_With what you know of the difference between these types of numbers, can you explain why the year 2010 in “January 1, 2010,” really is 2010 and not 2009? (Hint: you can’t pick years at random.)
+
+#A2_
+#Because there is no year 0. Thus the calendar year is meant to be "ordinal".
