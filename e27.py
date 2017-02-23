664f3a611509689cdba80a36e2e4f69babc3085b
rance <vergil000@gmail.com>
2017-02-23 10:31:29 +0900

    e27

---
+++ e27.py
@@ -0,0 +1,80 @@
+!= #Not Equal
+
+or = if there is a T, then T
+and = if there is a F, then F
+!or = if there is a T, then F
+!and = if there is a F, then T
+
+	or	and	!or	!and
+TF	T 	F	F	T
+TT	T 	T　	F	F
+FT	T 	F 	F 	T
+FF	F 	F 	T 	T
+
+	or	and	!or	!and
+TF	T 	F	F	T 
+FT	T 	F 	F 	T
+FF	F 	F 	T 	T
+TT	T 	T 	F 	F
+
+	or	and	!or	!and
+TF	T 	F 	F 	T
+FT	T 	F 	F 	T
+TT	T 	T 	F 	F
+FF	F 	F 	T 	T
+
+	or	and	!or	!and
+FF	F 	F 	T 	T 
+TT	T 	T 	F 	F
+TF 	T 	F 	F 	T
+FT	T 	F 	F 	T
+
+	or	and	!or	!and
+FF	F 	F 	T 	T
+TT	T 	T 	F 	F
+FT	T 	F 	F 	T
+TF 	T 	F 	F 	T
+
+	or	and	!or	!and
+FF	F 	F 	T 	T
+FT 	T 	F 	F 	T
+TF 	T 	F 	F 	T
+TT 	T 	T 	F 	F
+
+	or	and	!or	!and
+FT	T 	F 	F 	T
+FF  	F 	F 	T 	T
+TF	T 	F 	F 	T
+TT	T 	T 	F 	F
+
+	or	and	!or	!and
+FT 	T 	F 	F 	T
+FF 	F 	F 	T 	T
+TF 	T 	F 	F 	T
+TT 	T 	T 	F 	F
+
+	or	and	!or	!and
+FT 	T 	F 	F 	T
+TT	T 	T 	F 	F
+FF 	F 	F 	T 	T
+TF 	T 	F 	F 	T
+
+
+	or	and	!or	!and
+TF	T 	F 	F 	T
+FT 	T 	F 	F 	T
+TT 	T 	T 	F 	F
+FF	F 	F 	T 	T
+
+	or	and	!or	!and
+TF 	T 	F 	F 	T
+TT 	T 	T 	F 	F
+FT 	T 	F 	F 	T
+FF 	F 	F 	T 	T
+
+	or	and	!or	!and
+TF 	T 	F 	F 	T
+FF 	F 	F 	T 	T
+TT 	T 	T 	F 	F
+FT 	T 	F 	F 	T
+
