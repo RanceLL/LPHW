fb2ea74b8b44a5cd5102709a37bb92d53553985b
rance <vergil000@gmail.com>
2017-02-25 16:33:56 +0900

    e36

--- e35.py
+++ e35.py
@@ -3,6 +3,7 @@ def gold_room():
 	print "This room is full of gold. How much do you take?"
 
 	next = raw_input("---> ")
+
 	if "0" in next or "1" in next: 
 		how_much = int(next)
 	else:
@@ -14,6 +15,7 @@ def gold_room():
 #	if how_much < 50:		
 		print "Nice, you're not greedy, you win!"
 		exit(0)
+
 	else:
 		dead("You greedy bastard!")
 
@@ -26,17 +28,22 @@ def bear_room():
 
 	while True:    #"While True:" would make an INFINITE loop
 		next = raw_input("---> ")
+
 		if next == "take honey":
 			dead("The bear looks at you then slaps your face off.")
+		
 		elif next == "taunt bear" and not bear_moved:
 			print "The bear has moved from the door. You can go through it now."
 			bear_moved = True
 			#First time taunting. Python would stop running blocks here since Python starts at the top and runs the first block that is True
+		
 		elif next == "taunt bear" and bear_moved:
 			dead("The bear gets pissed off and chews you leg off.")
 			#Second time tauning. Since "bear_moved = True" now, Python would skip the block  above(which is the first "taunt bear" block.) So users can taunt bear for only ONE time.
+		
 		elif next == "open door" and bear_moved:
 			gold_room()
+		
 		else:
 			print "I got no idea what that means."
 
@@ -49,8 +56,10 @@ def cthulhu_room():
 
 	if "flee" in next:
 		start()
+	
 	elif "head" in next:
 		dead("Well that was tasty!")
+	
 	else:
 		cthulhu_room()
 
@@ -67,8 +76,10 @@ def start():
 
 	if next == "left":
 		bear_room()
+	
 	elif next == "right":
 		cthulhu_room()
+	
 	else:
 		dead("You stumble around the room until you starve.")
 
@@ -77,3 +88,8 @@ start()
 #Q1_Draw a map of the game and how you flow through it.
 #A1_See e35_map.png
 
+#TIPS
+#1. Every if-statement must have an else.
+#2. Never nest if-statements more than two deep and always try to do them one deep.
+#3. Treat if-statements like paragraphs, where each if, elif, else grouping is like a set of sentences. Put blank lines before and after.
+#4. Boolean tests should be simple.
---
+++ e36.py
@@ -0,0 +1,13 @@
+#About If-Statements
+1. Every if-statement must have an else.
+2. Never nest if-statements more than two deep and always try to do them one deep.
+3. Treat if-statements like paragraphs, where each if, elif, else grouping is like a set of sentences. Put blank lines before and after.
+4. Boolean tests should be simple.
+##About Loops
+5. Use a for-loop for all kinds of looping instead of using a while-loop.
+##About Debugging
+6. Do not use any debuggers.
+7. Best way to debug: use PRINT to print out the values of variables at points in the program to see what's wrong.
+8. Make sure parts of your programs work as you work on them. Code a little, run a little, fix a little.
+##General
+0. start doing it, do a small version, make it bigger, keep a list of things to do, and do them.
